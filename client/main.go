1912d7d9fe26365dfd1aece0268ebb886af9b460
Nerdy Gedoni <gedoniani@gmail.com>
2025-05-28 09:56:30 +0100

    refactor: build Windows-compatible VPN

--- client/main.go
+++ client/main.go
@@ -1,88 +1,62 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
 	"os"
-	"os/signal"
-	"syscall"
+	"strings"
 
 	"github.com/gedons/go_VPN/internal/config"
 	"github.com/gedons/go_VPN/internal/crypto"
-	"github.com/gedons/go_VPN/internal/tun"
 )
 
 func main() {
-	// Load config
 	conf, err := config.LoadClientConfig("configs/client-config.yaml")
 	if err != nil {
 		log.Fatalf("Failed to load config: %v", err)
 	}
 
-	// Create TUN interface
-	iface, err := tun.CreateTUNInterface(conf.TUNName)
-	if err != nil {
-		log.Fatalf("Failed to create TUN interface: %v", err)
-	}
-	defer iface.Close()
-
-	// Setup UDP connection to server
 	serverAddr := net.JoinHostPort(conf.ServerIp, fmt.Sprintf("%d", conf.ServerPort))
 	conn, err := net.Dial("udp", serverAddr)
 	if err != nil {
-		log.Fatalf("Failed to connect to VPN server: %v", err)
+		log.Fatalf("UDP dial error: %v", err)
 	}
 	defer conn.Close()
 
-	// Init encryption
 	cipher, err := crypto.NewCipher([]byte(conf.PSK))
 	if err != nil {
-		log.Fatalf("Failed to initialize cipher: %v", err)
+		log.Fatalf("Cipher init error: %v", err)
 	}
 
-	log.Println("VPN client started and connected to server.")
+	fmt.Println("Connected to VPN server. Type messages to send securely.")
+	reader := bufio.NewReader(os.Stdin)
 
-	// Graceful shutdown on Ctrl+C
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-sig
-		log.Println("Shutting down VPN client...")
-		os.Exit(0)
-	}()
-
-	// Read from TUN and send encrypted to server
-	go func() {
-		buf := make([]byte, 1500)
-		for {
-			n, err := iface.Read(buf)
-			if err != nil {
-				log.Printf("Error reading from TUN: %v", err)
-				continue
-			}
-			enc, err := cipher.Encrypt(buf[:n])
-			if err != nil {
-				log.Printf("Encrypt error: %v", err)
-				continue
-			}
-			_, _ = conn.Write(enc)
-		}
-	}()
-
-	// Read from server and write decrypted to TUN
-	buf := make([]byte, 1500)
+	// Send input from user
 	for {
-		n, err := conn.Read(buf)
+		fmt.Print("> ")
+		text, _ := reader.ReadString('\n')
+		text = strings.TrimSpace(text)
+
+		if text == "exit" {
+			break
+		}
+
+		encrypted, err := cipher.Encrypt([]byte(text))
 		if err != nil {
-			log.Printf("Error reading from server: %v", err)
+			log.Printf("Encrypt error: %v", err)
 			continue
 		}
-		dec, err := cipher.Decrypt(buf[:n])
-		if err != nil {
-			log.Printf("Decrypt error: %v", err)
-			continue
+
+		_, _ = conn.Write(encrypted)
+
+		// Read response
+		resp := make([]byte, 1500)
+		n, err := conn.Read(resp)
+		if err == nil {
+			decrypted, _ := cipher.Decrypt(resp[:n])
+			fmt.Println("Server:", string(decrypted))
 		}
-		_, _ = iface.Write(dec)
 	}
 }
